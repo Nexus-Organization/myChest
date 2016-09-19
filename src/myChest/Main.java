621a9ca41c97199efa7ad7bdc0658784b3f8ab01
angelless <dongkyu91622@gmail.com>
2016-09-20 02:19:57 +0900

    Main
    Commit

---
+++ src/myChest/Main.java
@@ -0,0 +1,17 @@
+package myChest
+
+public class Main extends PluginBase implements Listener{
+
+@Override
+public void onEnable(){
+this.getServer().getPluginManager().registerEvents(this,this);
+}
+
+
+
+
+
+
+
+
+}
